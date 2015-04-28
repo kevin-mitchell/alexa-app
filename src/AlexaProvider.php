5212cb6d1f78b2114b177099949f34994267c61d
develpr <kevin@develpr.com>
2015-04-28 12:17:46 -0400

    fixed typo and added bindings to service provider

--- src/AlexaProvider.php
+++ src/AlexaProvider.php
@@ -1,7 +1,6 @@
 <?php namespace Develpr\AlexaApp;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\ServiceProvider;
 
 class AlexaProvider extends ServiceProvider
@@ -21,6 +20,7 @@ class AlexaProvider extends ServiceProvider
     {
         $request = $this->app->make('request');
 
+		$this->bindAlexaRequest($request);
         $this->setupSession($request);
     }
 
@@ -35,10 +35,31 @@ class AlexaProvider extends ServiceProvider
         if( ! $sessionAttributes )
             return;
             
-        foreach($sessionAttribute as $key => $value)
+        foreach($sessionAttributes as $key => $value)
         {
             \Session::put($key, $value);
         }
 
     }
+
+	private function bindAlexaRequest(Request $request)
+	{
+		$this->app->bind('Develpr\AlexaApp\Request\AlexaRequest', function() use ($request) {
+
+			$requestType = array_get(json_decode($request->getContent(), true), 'request.type');
+
+			if($requestType == null)
+				$requestType = array_get(json_decode($request->input('content'), true), 'request.type');
+
+			$className = self::ALEXA_REQUEST_NAMESPACE . $requestType;
+
+			if( ! class_exists($className))
+			{
+				throw new \Exception("This type of request is not supported");
+			}
+
+			return new $className($request);
+
+		});
+	}
 }

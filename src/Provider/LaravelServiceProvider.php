9a873c4b6b03d849fc5ae5a2269225ebd5264d6e
develpr <kevin@develpr.com>
2015-06-21 12:02:28 -0400

    Changed migration name to be less pretty and more worky
    I didn't realize (somehow) migrations needed to have a certain file name. Oops.

--- src/Provider/LaravelServiceProvider.php
+++ src/Provider/LaravelServiceProvider.php
@@ -12,7 +12,7 @@ class LaravelServiceProvider extends ServiceProvider {
 			realpath(__DIR__.'/../../config/alexa.php') => config_path('alexa.php'),
 		], 'config');
 		$this->publishes([
-			realpath(__DIR__.'/../../database/create_alexa_devices_table.php') => database_path('/migrations/create_alexa_devices_table.php'),
+			realpath(__DIR__.'/../../database/2015_06_21_000000_create_alexa_devices_table.php') => database_path('/migrations/2015_06_21_000000_create_alexa_devices_table.php'),
 		], 'migrations');
 	}
 

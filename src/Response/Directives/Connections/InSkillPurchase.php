9dd861e7c25e5ce13a3638e0fbf6efce23a95683
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2019-11-14 16:47:49 +0100

    Refunds for ISP

--- src/Response/Directives/Connections/InSkillPurchase.php
+++ src/Response/Directives/Connections/InSkillPurchase.php
@@ -2,22 +2,22 @@
 
 namespace Develpr\AlexaApp\Response\Directives\Connections;
 
-
 class InSkillPurchase
 {
-
     private $productId;
     private $token;
+    private $type;
 
-    public function __construct($productId, $token = '')
+    public function __construct($productId, $token = '', $type = 'Buy')
     {
         $this->productId = $productId;
         $this->token = $token;
+        $this->type = $type;
     }
 
     public function asDirective()
     {
-        return new SendRequest('Buy', ['InSkillProduct' => ['productId' => $this->productId]], $this->token);
+        return new SendRequest($this->type, ['InSkillProduct' => ['productId' => $this->productId]], $this->token);
     }
 
     public function toArray()

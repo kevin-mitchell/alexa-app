13377a59b7f4f20c0bd4b19d470778a96e21dd25
Wojciech Szczurek <szczurek.wojciech@gmail.com>
2020-10-09 14:40:27 -0500

    chore: extends ProxyController

--- src/Controllers/ProxyController.php
+++ src/Controllers/ProxyController.php
@@ -2,8 +2,16 @@
 
 namespace Develpr\AlexaApp\Controllers;
 
-class ProxyController
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+use Illuminate\Routing\Controller;
+
+class ProxyController extends Controller
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
     public function audiofile($audiofile)
     {
         return response(base64_decode($audiofile))
